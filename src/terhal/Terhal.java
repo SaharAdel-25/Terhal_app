6bcfcc25ed5f7e58ce06fb8f21015977ae23d54d
SaharAdel-25 <soeaix@gmail.com>
2025-02-27 22:19:14 +0300

    Update Terhal.java

--- src/terhal/Terhal.java
+++ src/terhal/Terhal.java
@@ -7,18 +7,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *
- * @author janaz
- */
 public class Terhal {
 
-    /**
-     * @param args the command line arguments
-     */
+ 
 public static void main(String[] args) throws SQLException {
         // Assume the connection is automatically provided by NetBeans (verify with your NetBeans settings)
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/travel_app", "root", "Janajgsz2004");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/travel_app", "root", "password");
 
        SwingUtilities.invokeLater(() -> {
             TravelAppGUI appGUI = new TravelAppGUI(connection);
