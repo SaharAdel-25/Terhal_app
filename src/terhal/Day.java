ccb00120b8b6fe070e12e553f323cd75df99dfb8
SaharAdel-25 <soeaix@gmail.com>
2025-02-27 22:17:55 +0300

    Update Day.java

--- src/terhal/Day.java
+++ src/terhal/Day.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package terhal;
 
-/**
- *
- * @author janaz
- */
 public class Day {
     private String dayName;
     private Integer morningActivity;
